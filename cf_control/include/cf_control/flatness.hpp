d254eda06fdc71e9805d15c618fae419f1ecb2fb
Maziorow <410372@student.agh.edu.pl>
2026-04-01 13:00:31 +0200

    Need to write flatness node

---
+++ cf_control/include/cf_control/flatness.hpp
@@ -0,0 +1,15 @@
+#ifndef CF_CONTROL_FLATNESS_HPP_
+#define CF_CONTROL_FLATNESS_HPP_
+
+#include <cf_control_msgs/msg/drone_state.hpp>
+#include <cf_control_msgs/msg/thrust_and_torque.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
+
+namespace evs
+{
+namespace cf
+{
+
+}
+}
